8d582c56c6c52d2456c25306aac266f45b74087a
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2022-10-24 17:24:30 +0530

    Updated weather util function for getting weather forcast message

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const geoCode = require('./utils/geocode.js');
+const weather = require('./utils/weather.js');
 
 geoCode.getGeoCode('New Delhi', (error, result) => {
     if (error) {
@@ -7,3 +8,11 @@ geoCode.getGeoCode('New Delhi', (error, result) => {
         console.log(result);
     }
 })
+
+weather.getForcast(28.557163, 77.163665, (error, result) => {
+    if (error) {
+        console.log(error.message)
+    } else {
+        console.log(result);
+    }
+}) 
--- utils/weather.js
+++ utils/weather.js
@@ -1,14 +1,34 @@
 const axios = require('axios');
 const apiKeys = require('../api-keys.js')
 
+const forcast = (lat, lng, callback) => {
+    axios.get('http://api.weatherstack.com/current', {
+        params: {
+            access_key: apiKeys.weatherStackAccessKey,
+            query: `${lat},${lng}`
+        }   
+    })
+    .then((response) => {
+        if (response.status === 200) {
+            callback(
+                undefined, 
+                `Summery: ${response.data.current.weather_descriptions[0]}. Current Temprature is ${response.data.current.temperature} degree celsius. There is ${response.data.current.precip}% chance of rain.`
+            )
+        } else {
+            callback({
+                message: 'Something went wrong',
+                error: response,
+            }, undefined)
+        }
+    })
+    .catch((error) => {
+        callback({
+            message: 'Something went wrong',
+            error: error,
+        }, undefined)
+    })
+}
 
-// axios.get('http://api.weatherstack.com/current', {
-//     params: {
-//         access_key: apiKeys.weatherStackAccessKey,
-//         query: 'New Delhi'
-//     }   
-// }).then((response) => {
-//     console.log(response.data.current);
-// }).catch((err) => {
-//     console.log(err);
-// })
+module.exports = {
+    getForcast: forcast,
+};
